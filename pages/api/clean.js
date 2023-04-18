89c8c8124cfaafbe031273ea2c99a28a401b0c16
Dallin Bartholomew <dallinbart@gmail.com>
2023-04-18 15:06:54 -0600

    headers and entries parsed

--- pages/api/clean.js
+++ pages/api/clean.js
@@ -1,4 +1,22 @@
+import { parse } from 'csv-parse';
+
+let headers
+let entries
+
+async function parseCSV(csvString) {
+    parse(csvString, (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            [headers, ...entries] = data;
+            console.log('Headers:', headers);
+            console.log('Entries:', entries);
+        }
+    });
+}
+
 export default async function clean(content) {
+    await parseCSV(content);
     // clean and modify content
     return content;
 }
