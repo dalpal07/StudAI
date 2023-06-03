b925502d6929ec66e4ed0c55c5f5a57d1c4bf5f4
Dallin Bartholomew <dallinbart@gmail.com>
2023-06-03 15:33:19 -0600

    quick fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import {Head} from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <link rel="icon" href="/images/favicon.svg" />
+        <title>StudAI</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
 }
--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-        <Head>
-            <link rel="icon" href="/images/favicon.svg" />
-            <title>StudAI</title>
-        </Head>
       <body>
         <Main />
         <NextScript />
