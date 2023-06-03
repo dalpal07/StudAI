dcdfd71b5f2915a75f1471bd1dc1847afce21e16
Dallin Bartholomew <dallinbart@gmail.com>
2023-06-03 15:28:56 -0600

    added

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <link rel="icon" href="/images/favicon.svg" />
+            <title>StudAI</title>
+        </Head>
       <body>
         <Main />
         <NextScript />
