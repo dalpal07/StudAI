587e3da662103f27f5926c1b9fd09d818734aaa1
Dallin Bartholomew <dallinbart@gmail.com>
2023-06-29 20:35:54 -0600

    chat kinda working

---
+++ pages/api/chat.js
@@ -0,0 +1,36 @@
+let prompt = "You are an AI chatbot named Stud. Your goal is to chat with users about their data requests until you sufficiently understand the details of what they're asking. Be sure to get the exact table name(s) and appropriate header, column, or row names/indexes as well. When you sufficiently understand, simply tell the user that you will take care of the request. DO NOT explain how you will carry out the request.\n\nRespond to this conversation:\n"
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const body = JSON.parse(req.body);
+        body.forEach((line) => {
+            if (line.type === "user") {
+                prompt += "\nUser: " + line.message
+            } else {
+                prompt += "\nStud: " + line.message
+            }
+        })
+        prompt += "\nStud: [insert]"
+
+        const { Configuration, OpenAIApi } = require("openai");
+
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            suffix: "",
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        res.status(200).json({response: response.data.choices[0].text});
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import {Container} from "@mui/material";
 import {useState} from "react";
 import Instruction from "@/public/components/Instruction";
 import FileUpload from "@/public/components/FileUpload";
+import Chat from "@/public/components/Chat";
 
 export default function Home() {
     const [instruction, setInstruction] = useState("");
     return (
         <Container>
-            <Instruction instruction={instruction} setInstruction={setInstruction}/>
-            <FileUpload instruction={instruction}/>
+            {/*<Instruction instruction={instruction} setInstruction={setInstruction}/>*/}
+            {/*<FileUpload instruction={instruction}/>*/}
+            <Chat/>
         </Container>
     )
 }
