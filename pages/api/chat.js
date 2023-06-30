4453ed954ebec95c0dfd56f5912ce2d6119668f6
Dallin Bartholomew <dallinbart@gmail.com>
2023-06-30 12:38:40 -0600

    here we go

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,6 +1,5 @@
-let prompt = "You are an AI chatbot named Stud. Your goal is to chat with users about their data requests until you sufficiently understand the details of what they're asking. Be sure to get the exact table name(s) and appropriate header, column, or row names/indexes as well. When you sufficiently understand, simply tell the user that you will take care of the request. DO NOT explain how you will carry out the request.\n\nRespond to this conversation:\n"
-
 export default async function handler(req, res) {
+    let prompt = "You are an AI chatbot named Stud. Your goal is to chat with users about their data requests until you sufficiently understand the details of what they're asking. Be sure to get the exact table name(s) and appropriate header, column, or row names/indexes as well. When you sufficiently understand, simply tell the user that you will take care of the request. DO NOT explain how you will carry out the request.\n\nRespond to this conversation:\n"
     if (req.method === 'POST') {
         const body = JSON.parse(req.body);
         console.log("Prompt: " + prompt)
@@ -22,23 +21,17 @@ export default async function handler(req, res) {
         });
         const openai = new OpenAIApi(configuration);
 
-        // await openai.createCompletion({
-        //     model: "text-davinci-003",
-        //     prompt: prompt,
-        //     suffix: "",
-        //     temperature: 1,
-        //     max_tokens: 256,
-        //     top_p: 1,
-        //     frequency_penalty: 0,
-        //     presence_penalty: 0,
-        // }).then((response) => {
-        //     console.log("Response: " + response.data.choices[0].text);
-        //     res.status(200).json({response: response.data.choices[0].text});
-        // }).catch((err) => {
-        //     console.log("Error: " + err);
-        //     res.status(500).send('Internal Server Error');
-        // });
-        res.status(200).json({response: "Hello"});
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            suffix: "",
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        })
+        res.status(200).json({response: response.data.choices[0].text});
     } else {
         res.status(405).send('Method Not Allowed');
     }
