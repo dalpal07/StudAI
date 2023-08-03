a528386380d2f115ed30ed51cb24aefc7fc6de4a
Dallin Bartholomew <dallinbart@gmail.com>
2023-08-03 17:28:02 -0600

    whitelist working

---
+++ pages/BetaAnnouncement.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { BoldText } from "/public/components/common/Typographies";
+import {HeightSpacer} from "@/public/components/common/Spacers";
+import {GreenButton} from "@/public/components/common/Buttons";
+import {InnerBox} from "@/public/components/common/Boxes";
+
+export default function BetaAnnouncement() {
+    return (
+        <InnerBox>
+            <BoldText size={"1.125rem"}>Welcome to</BoldText>
+            <HeightSpacer height={"1.1rem"}/>
+            <Image src={"./images/FullLogo.svg"} alt={"StudAI Logo"} width={400} height={72.42}/>
+            <HeightSpacer height={"2rem"}/>
+            <BoldText size={"1.75rem"}>Your Personal Data Maid</BoldText>
+            <HeightSpacer height={"4.125rem"}/>
+            <a href="/api/auth/login" style={{textDecoration: "none"}}>
+                <GreenButton size={"1.125rem"} padding={"0.25rem 1.5rem"}>
+                    See what Stud can do
+                </GreenButton>
+            </a>
+        </InnerBox>
+    )
+}
--- public/components/conditionals/Page.js
+++ public/components/conditionals/Page.js
@@ -1,10 +1,37 @@
 import Product from "@/pages/Product";
 import Intro from "@/pages/Intro";
+import BetaAnnouncement from "@/pages/BetaAnnouncement";
+import {useState} from "react";
+
+async function getIsWhiteListed(user) {
+    const response = await fetch("/api/whitelist", {
+        method: "POST",
+        body: JSON.stringify(user)
+    })
+    if (response.status === 200) {
+        const data = await response.json()
+        return data.isWhiteListed
+    }
+    else {
+        return false
+    }
+}
 
 export default function Page(props) {
-    if (props.user) return (
-        <Product/>
-    )
+    const [isWhiteListed, setIsWhiteListed] = useState(false)
+    if (props.user) {
+        getIsWhiteListed(props.user).then(data => {
+            setIsWhiteListed(data)
+        })
+        if (isWhiteListed) {
+            return (
+                <Product/>
+            )
+        }
+        return (
+            <BetaAnnouncement/>
+        )
+    }
     return (
         <Intro/>
     )
