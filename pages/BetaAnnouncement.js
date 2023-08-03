6f9cbf3798393c7537ec78d8fd7149915c9ed648
Dallin Bartholomew <dallinbart@gmail.com>
2023-08-03 17:31:34 -0600

    simple beta page

--- pages/BetaAnnouncement.js
+++ pages/BetaAnnouncement.js
@@ -7,17 +7,9 @@ import {InnerBox} from "@/public/components/common/Boxes";
 export default function BetaAnnouncement() {
     return (
         <InnerBox>
-            <BoldText size={"1.125rem"}>Welcome to</BoldText>
-            <HeightSpacer height={"1.1rem"}/>
-            <Image src={"./images/FullLogo.svg"} alt={"StudAI Logo"} width={400} height={72.42}/>
-            <HeightSpacer height={"2rem"}/>
-            <BoldText size={"1.75rem"}>Your Personal Data Maid</BoldText>
-            <HeightSpacer height={"4.125rem"}/>
-            <a href="/api/auth/login" style={{textDecoration: "none"}}>
-                <GreenButton size={"1.125rem"} padding={"0.25rem 1.5rem"}>
-                    See what Stud can do
-                </GreenButton>
-            </a>
+            <BoldText size={"3rem"}>Get excited for Beta</BoldText>
+            <HeightSpacer size={"1rem"}/>
+            <BoldText size={"1.5rem"}>Coming September 2023</BoldText>
         </InnerBox>
     )
 }
