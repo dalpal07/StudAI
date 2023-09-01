e9a7bf7125e827500e20e59bef860f7b5de6e481
Dallin Bartholomew <dallinbart@gmail.com>
2023-09-01 12:50:17 -0600

    file content appears

--- public/components/product/ViewData.js
+++ public/components/product/ViewData.js
@@ -1,18 +1,57 @@
 import {BoldText, GrayBoldText} from "@/public/components/common/Typographies";
 import {HeightSpacer} from "@/public/components/common/Spacers";
 import {WhiteButton} from "@/public/components/common/Buttons";
-import {ViewDataBox} from "@/public/components/common/Boxes";
+import {TableContainer, ViewDataBox} from "@/public/components/common/Boxes";
+import {useSelector} from "react-redux";
+import {selectCurrentFileEntries, selectCurrentFileHeaders, selectCurrentFileName} from "@/slices/fileSlice";
+import {Table, TableBody, TableHead, TableRow} from "@mui/material";
+import {Cell} from "@/public/components/common/Miscellaneous";
 
 export default function ViewData() {
+    const currentFileName = useSelector(selectCurrentFileName);
+    const currentFileHeaders = useSelector(selectCurrentFileHeaders);
+    const currentFileEntries = useSelector(selectCurrentFileEntries);
     return (
         <ViewDataBox>
-            <BoldText size={"1.125rem"}>View data set here</BoldText>
-            <HeightSpacer height={"0.5rem"}/>
-            <GrayBoldText size={"0.875rem"}>Drag and drop from ‘My data sets’ or</GrayBoldText>
-            <HeightSpacer height={"1.5rem"}/>
-            <WhiteButton>
-                <BoldText size={"0.875rem"}>Select from my data sets</BoldText>
-            </WhiteButton>
+            {
+                currentFileName ?
+                    <>
+                        <TableContainer>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        {currentFileHeaders.map((header, index) => (
+                                            <Cell key={index} rowIndex={0}>
+                                                {header}
+                                            </Cell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {currentFileEntries.map((entry, rowIndex) => (
+                                        <TableRow key={rowIndex}>
+                                            {entry.map((cell, cellIndex) => (
+                                                <Cell key={cellIndex}>
+                                                    {cell}
+                                                </Cell>
+                                            ))}
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </>
+                    :
+                    <>
+                        <BoldText size={"1.125rem"}>View data set here</BoldText>
+                        <HeightSpacer height={"0.5rem"}/>
+                        <GrayBoldText size={"0.875rem"}>Drag and drop from ‘My data sets’ or</GrayBoldText>
+                        <HeightSpacer height={"1.5rem"}/>
+                        <WhiteButton>
+                            <BoldText size={"0.875rem"}>Select from my data sets</BoldText>
+                        </WhiteButton>
+                    </>
+            }
         </ViewDataBox>
     )
 }
